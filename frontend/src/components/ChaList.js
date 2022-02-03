93a554574e39b3a0018902ce79cc755f61e6a98d
Udit <royalboy0012@gmail.com>
2022-02-03 20:30:05 +0530

    making it better

--- frontend/src/components/ChaList.js
+++ frontend/src/components/ChaList.js
@@ -19,7 +19,7 @@ const ChatList = (props) => {
   const onClose = () => {
     setOpendialog(!opendialog)
   }
-  const {link, fetchagain, user, logout, showAlert, chats, setChats, selectedchat, setSelectedchat } = useContext(NoteContext)
+  const { link, fetchagain, user, logout, showAlert, chats, setChats, selectedchat, setSelectedchat } = useContext(NoteContext)
 
   const [openMenu, setOpenMenu] = useState(false);
   const [drawer, setDrawer] = useState(false);
@@ -29,12 +29,12 @@ const ChatList = (props) => {
   const displayMenu = () => {
     setOpenMenu(!openMenu)
   }
-  useEffect(() => {
-    if(openMenu){
-      window.addEventListener("click",displayMenu())
+  window.addEventListener("click", function () {
+    if (openMenu) {
+      displayMenu()
     }
-  }, []);
-  
+  })
+
   const displayDrawer = () => {
     setDrawer(!drawer)
   }
@@ -66,8 +66,8 @@ const ChatList = (props) => {
         <div className='topbarHead' style={{ fontSize: isMobile ? "1.3rem" : istablet ? "1.5rem" : "1.6rem", margin: isMobile ? ".3em .6em" : ".7em .6em" }}>Recent Chats</div>
         {!isMobile && <button className='lbtn disflex' onClick={opengroupmodal} style={{ fontSize: '.8rem', backgroundColor: "white", color: appcolor, marginRight: ".6em" }}><span className='material-icons' style={{ color: appcolor, marginRight: ".2em", fontSize: "1.3rem" }}>add</span> Create Group</button>}
         {isMobile && <div className='disflex' style={{ marginRight: ".6em" }}>
-          <span className='material-icons' onClick={displayDrawer} style={{ color: "white", fontSize: "2rem", marginRight: ".4em",cursor:"pointer" }}>search</span>
-          <span className='material-icons' onClick={opengroupmodal} style={{ color: "white", fontSize: "2rem", marginRight: ".4em",cursor:"pointer" }}>add</span>
+          <span className='material-icons' onClick={displayDrawer} style={{ color: "white", fontSize: "2rem", marginRight: ".4em", cursor: "pointer" }}>search</span>
+          <span className='material-icons' onClick={opengroupmodal} style={{ color: "white", fontSize: "2rem", marginRight: ".4em", cursor: "pointer" }}>add</span>
           <div style={{ position: "relative" }}>
             <img onClick={displayMenu} className='profileimg' style={{ width: "40px", height: "40px" }} src={localStorage.getItem("token") ? user.pic : "https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg"} />
             <ul className='menubox' style={{ display: openMenu ? "block" : "none" }}>
@@ -87,7 +87,7 @@ const ChatList = (props) => {
           </div>}
           {chats ? (<div className='w100 disflex' style={{ justifyContent: "flex-end", flexDirection: "column-reverse", height: "93%", overflow: "auto" }}>
             {chats?.map((chat) => {
-              return <div key={chat._id} onClick={() => {setSelectedchat(chat);}} className='contactCard' style={selectedchat === chat ? { backgroundColor: appcolor, color: "white" } : {}}>
+              return <div key={chat._id} onClick={() => { setSelectedchat(chat); }} className='contactCard' style={selectedchat === chat ? { backgroundColor: appcolor, color: "white" } : {}}>
                 <img className='profileimg' src={!chat.isGroupchat ? getSender(user, chat.users).pic : "https://www.pngitem.com/pimgs/m/78-786314_computer-user-icon-peolpe-avatar-group-people-avatar.png"} alt="" />
                 <div className='contactinfo'>
                   <span className='contactname' style={{ fontSize: isMobile ? "1.3rem" : "1.4rem", textTransform: "capitalize" }}>{chat.isGroupchat ? chat.chatname : getSender(user, chat.users).username}</span>
@@ -98,10 +98,10 @@ const ChatList = (props) => {
                 </div>
               </div>
             })}
-          </div>) : ( <div className='w100 disflex' style={{height:"93%"}}>
+          </div>) : (<div className='w100 disflex' style={{ height: "93%" }}>
             <div><span className='material-icons'>search</span>Search your friend</div>
             <div>Start a conversation on LetsChat</div>
-          </div> )}
+          </div>)}
         </div>
       </div>
     </div>
