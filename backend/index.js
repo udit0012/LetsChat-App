06760eb5f045f0bf626dfd2c9a07e0b3c907bccf
Udit <royalboy0012@gmail.com>
2022-08-18 18:52:59 +0530

    changed command

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ if(process.env.NODE_ENV==="production"){
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://letschat-with-friends.herokuapp.com",
     },
 });
 
