9064b06d0f74230f9ea0796cdc6779f0cbb90665
Udit <royalboy0012@gmail.com>
2022-08-31 00:52:33 +0530

    changed chatList.js

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -7,14 +7,6 @@ import Footer from '../components/miscellaneous/Footer';
 import { useMediaQuery } from 'react-responsive';
 
 const Homepage = () => {
-  const isMobile = useMediaQuery({ query: '(max-width: 700px)' })
-  const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   const user = JSON.parse(localStorage.getItem("user"));
-
-  //   if (user) navigate("/LetsChat");
-  // }, [navigate]);
   return <div>
       <div className="mainbody w100 h100">
             <Navbar />
